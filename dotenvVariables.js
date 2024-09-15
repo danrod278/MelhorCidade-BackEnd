9a509b3fbc8fd9b8ada0a89610fa67f46819e704
danrod278 <71533988+danrod278@users.noreply.github.com>
2024-09-15 20:51:01 -0300

    Update dotenvVariables.js

--- dotenvVariables.js
+++ dotenvVariables.js
@@ -1,16 +1,10 @@
 require('dotenv').config()
 
-/*exports.dotEnvVariables = {
+exports.dotEnvVariables = {
     SECRET_SESSION:process.env.SECRET_SESSION,
     ATLAS_URI_CONNECTION:process.env.ATLAS_URI_CONNECTION,
     CRIARUSUARIO_PORT:process.env.CRIARUSUARIO_PORT,
     SALTROUNDS:process.env.SALTROUNDS
-}*/
-
-exports.dotEnvVariables = {
-    SECRET_SESSION:"_albert15promax_",
-    ATLAS_URI_CONNECTION:"mongodb+srv://danrod278:qsc278279wdv@iris-vision.v0lmx6e.mongodb.net/?retryWrites=true&w=majority&appName=Iris-vision",
-    CRIARUSUARIO_PORT:3000,
-    SALTROUNDS:10
 }
+
 
