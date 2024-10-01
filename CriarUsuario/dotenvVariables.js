40108207768ce5c77cf6ba41bfa73f7c8cfd354b
danrod278 <71533988+danrod278@users.noreply.github.com>
2024-10-01 12:33:04 -0300

    Update dotenvVariables.js

--- CriarUsuario/dotenvVariables.js
+++ CriarUsuario/dotenvVariables.js
@@ -1,6 +1,6 @@
 exports.dotEnvVariables = {
     SECRET_SESSION:"",
-    ATLAS_URI_CONNECTION:"mongodb+srv://danrod278:qsc278279wdv@iris-vision.v0lmx6e.mongodb.net/?retryWrites=true&w=majority&appName=Iris-vision",
+    ATLAS_URI_CONNECTION:"",
     CRIARUSUARIO_PORT:3002,
     SALTROUNDS:10
 }
