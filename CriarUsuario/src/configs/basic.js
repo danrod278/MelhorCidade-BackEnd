6fb0ae6a93c1a80409b6d853c10848e858697f19
danrod278 <71533988+danrod278@users.noreply.github.com>
2024-10-30 14:16:40 -0300

    Update basic.js

--- CriarUsuario/src/configs/basic.js
+++ CriarUsuario/src/configs/basic.js
@@ -2,6 +2,16 @@ const express = require("express")
 
 const bodyParser = require('body-parser')
 const app = express()
+const cors = require("cors")
+
+app.use(cors({
+    origin: 'http://localhost:5173', 
+    methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+    allowedHeaders: ['Content-Type', 'Authorization'],
+    credentials: true
+}))
+
+app.options('*', cors())
 
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded( {extended: true}));
