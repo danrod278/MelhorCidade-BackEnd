18d42d2ff357ac81b95fe94a1c3220f195a4b2af
danrod278 <71533988+danrod278@users.noreply.github.com>
2024-10-30 14:18:32 -0300

    Update basic.js

--- ManipularPost/src/config/basic.js
+++ ManipularPost/src/config/basic.js
@@ -1,6 +1,16 @@
 const express = require("express")
 const app = express()
 const bodyParser = require("body-parser")
+const cors = require("cors")
+
+app.use(cors({
+    origin: 'http://localhost:5173', 
+    methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+    allowedHeaders: ['Content-Type', 'Authorization'],
+    credentials: true
+}))
+
+app.options('*', cors())
 
 app.use(bodyParser.urlencoded({limit:"7mb",extended:true}))
 app.use(bodyParser.json({limit:"7mb"}))
