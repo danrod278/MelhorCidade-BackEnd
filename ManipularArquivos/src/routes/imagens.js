9201bcad99a2a52449b1eeb1f254f77c16245e07
danrod278 <71533988+danrod278@users.noreply.github.com>
2024-10-30 21:05:51 -0300

    Update imagens.js

--- ManipularArquivos/src/routes/imagens.js
+++ ManipularArquivos/src/routes/imagens.js
@@ -3,6 +3,5 @@ const router = express.Router()
 const { salvarImagemController, lerImagensController } = require('../controller/manipularImagensControllers')
 
 router.post("/salvarImagem", salvarImagemController)
-router.post("/lerImagens", lerImagensController)
 
 module.exports = router
