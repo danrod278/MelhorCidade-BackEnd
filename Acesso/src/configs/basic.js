e6123b10988e532b7988bcf3f05c58b18408faa2
danrod278 <71533988+danrod278@users.noreply.github.com>
2024-11-02 20:20:43 -0300

    Update basic.js

--- Acesso/src/configs/basic.js
+++ Acesso/src/configs/basic.js
@@ -4,7 +4,7 @@ const cookieParser = require("cookie-parser")
 const app = express()
 const cors = require("cors")
 app.use(cors({
-    origin: 'http://localhost:5173', 
+    origin: 'http://melhorcidade.vercel.app', 
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
     allowedHeaders: ['Content-Type', 'Authorization'],
     credentials: true
