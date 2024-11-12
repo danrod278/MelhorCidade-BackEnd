efb1521204a8e02fdc41cacf434585ea40079e3f
danrod278 <71533988+danrod278@users.noreply.github.com>
2024-11-11 23:05:12 -0300

    Delete CriarUsuario directory

--- CriarUsuario/dotenvVariables.js
+++
@@ -1,7 +0,0 @@
-require('dotenv').config()
-
-exports.dotEnvVariables = {
-    ATLAS_URI_CONNECTION:process.env.ATLAS_URI_CONNECTION,
-    CRIARUSUARIO_PORT:process.env.PORT_CRIAR_USUARIO,
-    SALTROUNDS:process.env.SALTROUNDS
-}
--- CriarUsuario/src/routes/criarUsuario.js
+++
@@ -1,7 +0,0 @@
-const express = require("express")
-const router = express.Router()
-const {criarUsuarioController} = require('../controllers/criarUsuario')
-
-router.post("/criarConta", criarUsuarioController)
-
-module.exports = router
